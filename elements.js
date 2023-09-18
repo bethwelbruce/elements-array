fa63875130217f6124591e0ca814b094d4b8d94c
BETHWEL NELSON OTIENO <bethwelbruce@gmail.com>
2023-09-18 16:02:43 +0300

    elements.js

---
+++ elements.js
@@ -0,0 +1,14 @@
+function sum(array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum;
+}
+
+// Example usage:
+
+const array = [1, 2, 3, 4, 5];
+const sumOfArray = sum(array);
+
+console.log(sumOfArray); // 15
